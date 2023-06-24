432e4db6618d36e98d9bd01329ab252074eb2653
HakkiDogan <180316029@ogr.cbu.edu.tr>
2023-06-24 16:29:44 +0300

    Hata düzeltmeleri yapıldı

--- BusinessLayer/ValidationRules/FluentValidation/MemberValidator.cs
+++ BusinessLayer/ValidationRules/FluentValidation/MemberValidator.cs
@@ -16,9 +16,11 @@ namespace BusinessLayer.ValidationRules.FluentValidation
             RuleFor(m => m.UserName).NotEmpty().WithMessage("Kullanıcı adı boş bırakılamaz!");
             RuleFor(m => m.PhoneNumber).NotEmpty().WithMessage("Telefon numarası boş bırakılamaz!");
             RuleFor(m => m.Surname).NotEmpty().WithMessage("Üye soyadı boş bırakılamaz!");
-            RuleFor(m => m.School).NotEmpty().WithMessage("Okul adı boş bırakılamaz");
+            RuleFor(m => m.School).NotEmpty().WithMessage("Okul adı boş bırakılamaz!");
             RuleFor(m => m.School).MinimumLength(10).WithMessage("Okul adı en az 10 karakter olmalıdır!");
-            RuleFor(m => m.UserName).MinimumLength(5).WithMessage("Kullanıcı adı en az 5 karakter olmalıdır!");           
+            RuleFor(m => m.UserName).MinimumLength(5).WithMessage("Kullanıcı adı en az 5 karakter olmalıdır!");
+            RuleFor(m => m.Mail).NotEmpty().WithMessage("E-posta boş bırakılamaz!");
+            RuleFor(m => m.Password).NotEmpty().WithMessage("Şifre boş bırakılamaz!");
         }
     }
 }
